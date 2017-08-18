59bc38073a0fd4613021f011ee4f44a92ea36ecd
Junsoo Park <parkjunsoo91@gmail.com>
2017-08-18 12:54:30 +0900

    new crawler funtional

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ import http.client
 from urllib.parse import quote
 import json
 import sqlite3
-
+import datetime
 
 
 
@@ -72,7 +72,7 @@ def create_match_table():
 		ban8 integer,
 		ban9 integer,
 		ban10 integer,
-		data text
+		data text)
 		''')
 	connection.commit()
 
@@ -81,9 +81,9 @@ def exists_account_id(account_id, season_id = 0):
 	connection = sqlite3.connect('loldata2.db')
 	cur = connection.cursor()
 	if season_id == 0:
-		cur.execute("SELECT * FROM users WHERE aid = ?", account_id)
+		cur.execute("SELECT * FROM users WHERE aid = ?", (account_id,))
 	else:
-		cur.execute("SELECT * FROM users SELECT * WHERE aid=:aid and season{}=:1".format(season_id), {"aid":account_id})
+		cur.execute("SELECT * FROM users WHERE aid=:aid and season{}=:1".format(season_id), {"aid":account_id})
 	row = cur.fetchone()
 	if row == None:
 		return False
@@ -93,7 +93,7 @@ def exists_summoner_id(summoner_id, season_id = 0):
 	connection = sqlite3.connect('loldata2.db')
 	cur = connection.cursor()
 	if season_id == 0:
-		cur.execute("SELECT * FROM users WHERE sid = ?", summoner_id)
+		cur.execute("SELECT * FROM users WHERE sid = ?", (summoner_id,))
 	else:
 		cur.execute("SELECT * FROM users WHERE sid=:sid and season{}=1".format(season_id), {"sid":summoner_id})
 	row = cur.fetchone()
@@ -104,89 +104,69 @@ def exists_summoner_id(summoner_id, season_id = 0):
 def exists_match(game_id):
 	connection = sqlite3.connect('loldata2.db')
 	cur = connection.cursor()
-	cur.execute("SELECT * FROM matches WHERE gameID = ?", game_id)
+	cur.execute("SELECT * FROM matches WHERE gameId = ?", (game_id,))
 	row = cur.fetchone()
 	if row == None:
 		return False
 	return True
 
 def record_match(match_dto):
-	if exists_match():
+	if exists_match(match_dto["gameId"]):
 		return
-	values = {}
-	values['seasonId'] = match_dto['seasonId']
-	values['queueId'] = match_dto['queueId']
-	values['gameId'] = match_dto['gameId']
-	values['gameVersion'] = match_dto['gameVersion']
-	values['platformId'] = match_dto['platformId']
-	values['gameDuration'] = match_dto['gameDuration']
-	values['gameCreation'] = match_dto['gameCreation']
-	values['winner'] = 100 if match_dto['teams'][0]['win']=='Win' else 200
+	values = []
+	values.append(match_dto['gameId'])
+	values.append(match_dto['seasonId'])
+	values.append(match_dto['queueId'])
+	values.append(match_dto['gameVersion'])
+	values.append(match_dto['platformId'])
+	values.append(match_dto['gameDuration'])
+	values.append(match_dto['gameCreation'])
+	winner = 100 if match_dto['teams'][0]['win']=='Win' else 200
+	values.append(winner)
 	for i in range (10):
 		participant_identity_dto = match_dto['participantIdentities'][i]
-		participantId = participant_identity_dto['participantId']
-		values['accountId'+participantId] = participant_identity_dto['player']['accountId']
-
-		partcipant_dto = match_dto['participants'][i]
-		participantid = participant_dto['participantId']
-		values['pick'+participantId] = participant_dto['championId']
-
-		values['ban'+(i+1)] = 0
-
-	for team_stats_dto in match_dto['teams']:
-		for team_bans_dto in team_stats_dto['bans']:
-			values['ban'+team_bans_dto['pickTurn']] = team_bans_dto['championId']
-
-	values['data'] = json.dumps(match_dto)
+		values.append(participant_identity_dto['player']['accountId'])
+	for i in range(10):
+		participant_dto = match_dto['participants'][i]
+		values.append(participant_dto['championId'])
+	bans = []
+	for i in range(2):
+		team_stats_dto = match_dto['teams'][i]
+		for j in range(5):
+			if j < len(team_stats_dto['bans']):
+				bans.append(team_stats_dto['bans'][j]['championId'])
+			else:
+				bans.append(0)
+	values += bans
+	values.append(json.dumps(match_dto))
 
 	connection = sqlite3.connect('loldata2.db')
 	cur = connection.cursor()
 	cur.execute('''INSERT INTO matches VALUES (
-		gameId=:gameId
-		seasonId=:seasonId,
-		queueId=:queueId,
-		gameVersion=:gameVersion,
-		platformId=:platformId,
-		gameDuration=:gameDuration,
-		gameCreation=:gameCreation,
-		winner=:winner,
-		accountId1=:accountId1,	accountId2=:accountId2, accountId3=:accountId3,
-		accountId4=:accountId4, accountId5=:accountId5, accountId6=:accountId6,
-		accountId7=:accountId7, accountId8=:accountId8, accountId9=:accountId9,
-		accountId10=:accountId10,
-		pick1=:pick1, pick2=:pick2, pick3=:pick3, pick4=:pick4, pick5=:pick5,
-		pick6=:pick6, pick7=:pick7, pick8=:pick8, pick9=:pick9, pick10=:pick10,
-		ban1=:ban1, ban2=:ban2, ban3=:ban3, ban4=:ban4, ban5=:ban5,
-		ban6=:ban6, ban7=:ban7, ban8=:ban8, ban9=:ban9, ban10=:ban10,
-		data=:data)''', values)
+		?,?,?,?,?,?,?,?,
+		?,?,?,?,?,?,?,?,?,?,
+		?,?,?,?,?,?,?,?,?,?,
+		?,?,?,?,?,?,?,?,?,?,
+		?)''', tuple(values))
 	connection.commit()
 
-def record_user(account_id, summoner_id, tier, season):
+def record_user(account_id, summoner_id, tier, season_id):
 	connection = sqlite3.connect('loldata2.db')
 	cur = connection.cursor()
 	if exists_account_id(account_id) == False:
-		values = {}
-		values['aid'] = account_id
-		values['sid'] = summoner_id
-		values['tier'] = tier
-		cur.execute('''INSERT INTO matches VALUES (
-			aid=:aid,
-			sid=:sid,
-			tier=:tier,
-			season0=0,
-			season1=0,
-			season2=0,
-			season3=0,
-			season4=0,
-			season5=0,
-			season6=0,
-			season7=0,
-			season8=0,
-			season9=0)''', values)
+		values = []
+		values.append(account_id)
+		values.append(summoner_id)
+		values.append(tier)
+		values += [0] * 10
+		cur.execute('''INSERT INTO users VALUES (
+			?,?,?,
+			?,?,?,?,?,?,?,?,?,?
+			)''', tuple(values))
 		connection.commit()
 
 	cur = connection.cursor()
-	cur.execute("UPDATE accounts SET season{}=1 where aid = ?".format(seasonId), accountId)
+	cur.execute("UPDATE users SET season{}=1 where aid = ?".format(season_id), (account_id,))
 	connection.commit()
 
 
@@ -195,24 +175,31 @@ def record_user(account_id, summoner_id, tier, season):
 
 
 def get_summoner_by_account_id(account_id):
+	print("getting summoner of aid", account_id)
 	request_body = "/lol/summoner/v3/summoners/by-account/{}".format(account_id)
 	return send_request(request_body)
 	
 def get_summoner_by_summoner_id(summoner_id):
+	print("getting summoner of sid", summoner_id)
 	request_body = "/lol/summoner/v3/summoners/{}".format(summoner_id)
 	return send_request(request_body)
 
 def get_league(summoner_id):
+	print("getting league of sid", summoner_id)
 	request_body = "/lol/league/v3/leagues/by-summoner/{}".format(summoner_id)
 	return send_request(request_body)
 
 def get_matchlist(account_id, queue_id=0, season_id = 0):
+	print("getting matchlist for aid", account_id)
 	request_body = "/lol/match/v3/matchlists/by-account/{}".format(account_id)
 	if season_id != 0:
 		request_body = request_body + "?season=" + str(season_id)
 	return send_request(request_body)
 
 def get_match(game_id):
+	global counter
+	counter += 1
+	print("getting match of gameId", game_id, "(", counter, ")")
 	request_body = "/lol/match/v3/matches/{}".format(game_id)
 	return send_request(request_body)
 
@@ -227,7 +214,7 @@ def send_request(request_body):
 		try:
 			connection.request("GET", request_body, headers={'X-Riot-Token': key, })
 			response = connection.getresponse()
-			print(response.status, response.reason)
+			print(datetime.datetime.now(), response.status, response.reason)
 			if response.status == 200:
 				b = response.read()
 				dataObject = json.loads(b)
@@ -269,44 +256,50 @@ def collect_league(seed_id):
 			summoner_id = league_item_dto['playerOrTeamId']
 			if exists_summoner_id(summoner_id, season_id=SEASON_ID):
 				continue
+			print(SEED)
 			#api call for summoner info
-			account_id = get_summoner_by_summoner_id(summoner_id)
+			summoner_dto = get_summoner_by_summoner_id(summoner_id)
+			account_id = summoner_dto['accountId']
 			#api call for matchlist
 			matchlist_dto = get_matchlist(account_id, queue_id=[QUEUE_ID], season_id=SEASON_ID)
 			if matchlist_dto != None:
 				for match_reference_dto in matchlist_dto['matches']:
-					game_id = match_reference_dto['game_id']
+					game_id = match_reference_dto['gameId']
 					if exists_match(game_id):
 						continue
 					#api call for match
 					match_dto = get_match(game_id)
 					record_match(match_dto)
 					seed_summoner_ids += get_seed(match_dto)
-			record_user(account_id, summoner_id, tier, season)
+			record_user(account_id, summoner_id, tier, SEASON_ID)
 	return seed_summoner_ids
 
 def main():
 	#create_user_table()
 	#create_match_table()
-	seeds = [SILVER]
+	seeds = [SEED]
 	while True:
 		seed = seeds.pop(0)
 		new_seeds = collect_league(seed)
 		seeds = seeds + new_seeds
 
-
-API_NA = "RGAPI-8996de32-03b0-4029-bdf1-6ef1468a9966"
-API_KR1 = ""
-API_KR2 = ""
-API_KR3 = ""
-
-API_KEY = API_NA
-BRONZE = 123
+counter = 0
+BRONZE = 0
 SILVER = 2833703
-GOLD = 123
-PLATINUM = 123
-DIAMOND = 123
+GOLD = 0
+PLATINUM = 0
+DIAMOND = 2109280
+MASTER = 1229790
+CHALLENGER = 1222794
 SEASON_ID = 9
 QUEUE_ID = 420
+
+KEY_NA = "RGAPI-8996de32-03b0-4029-bdf1-6ef1468a9966"
+KEY_KR1 = "RGAPI-ed3577b4-fff3-4972-b66a-9d95c0d301f0"
+KEY_KR2 = "RGAPI-c0e0da6b-8272-4f0a-a47d-ec5fc5d7cd32"
+KEY_KR3 = "RGAPI-6fa1e75e-6810-4fe6-a7ff-f654e5e23613"
+
+API_KEY = KEY_KR3
+SEED = DIAMOND
 
 main()
